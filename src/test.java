f4cebfa6155ef42c33353beddeede9a353fb80bc
洪毓聰 <vipvip80@yahoo.com.tw>
2015-12-21 17:37:23 +0800

    three

--- src/test.java
+++ src/test.java
@@ -1,27 +1,34 @@
+import java.util.ArrayList;
+
 abstract class Questionlist{
-	
 	abstract void print();
 }
 class Question extends Questionlist{
 	   String description;
+	   ArrayList<Questionlist> list;
 	   public Question(String desc) {
 	      this.description = desc;
+	      list =new ArrayList();
 	   }
-	  
+	   public void addquestion(Questionlist c) {
+		   list.add(c);
+		   }
 	   public void print() {
 	      System.out.println(description);
-	   }
-	  
+	      for(Questionlist q:list)  
+	        {  
+	            q.print();  
+	        }  
+	   } 
 }
 
 class Leaf extends Questionlist{
 	void print(){
-		
 	}
 }
 public class test {
  public static void main(String[] args) {
-	 Questionlist q1=new  Question("1+1=?");
-	 q1.print();
+	 
+	
 	 }
 }
